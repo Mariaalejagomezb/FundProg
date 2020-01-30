86f7e01efb989bb2d22f4740cf35216534726647
Mariaalejagomezb <60151417+Mariaalejagomezb@users.noreply.github.com>
2020-01-30 01:27:15 -0500

    Add files via upload

---
+++ SalarioMinimo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace smmlv.ccf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte su salario en smmlv");
+            int salariominimo = 877803;
+            double smm = double.Parse(Console.ReadLine());
+
+            if (smm < salariominimo * 2)  {
+
+                Console.WriteLine("Tarifa A");
+
+            }
+
+            else if (salariominimo * 2<= smm && smm <= salariominimo * 4){
+
+                Console.WriteLine("Tarifa B");
+            }
+            else{
+
+                Console.WriteLine("Tarifa C");
+            }
+        }
+    }
+}
