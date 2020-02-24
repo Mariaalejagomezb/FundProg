31623872a933cf22592e2cf0d2cc5d9a22faf168
Mariaalejagomezb <60151417+Mariaalejagomezb@users.noreply.github.com>
2020-02-24 16:36:58 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace captcha2jugadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nombre = "";
+            Random aleatorio = new Random();
+            int puntos = 0;
+            int contador = 0;
+            int dado = 0;
+
+
+            Console.WriteLine("Ingrese el numero de jugador");
+            double n = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese los nombres");
+            nombre = Console.ReadLine();
+            Console.WriteLine("¿Deseas empezar con el juego?");
+            string r = Console.ReadLine();
+
+            while (r == "s" && puntos < 100)
+            {
+                Console.WriteLine("¿Tirar dados?(s/n)");
+                dado = aleatorio.Next(1, 7);
+                if (r == "n") break;
+                if (r == "s")
+                {
+                    Console.WriteLine("Dado" + dado);
+                    puntos += dado;
+                    if (dado == 1)
+                    {
+                        contador++;
+                        Console.WriteLine("Contador: " + contador);
+                        if (dado != 1) contador--;
+                        if (contador == 3)
+                        {
+                            Console.WriteLine("Has perdido");
+                            break;
+                        }
+                    }
+                    if (puntos == 100)
+                    {
+                        Console.WriteLine("Has ganado");
+                        break;
+
+                    }
+                }
+            }
+        }
+    }
+}
