e781b58d9bfb760e7982a3d16878c18310c9cadd
Mariaalejagomezb <60151417+Mariaalejagomezb@users.noreply.github.com>
2020-08-21 10:17:28 -0500

    Add files via upload

---
+++ BlackJackorden.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Numerics;
+
+namespace BlackJackOrden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            string mascartas = "";
+            Console.WriteLine("¡Empezamos!");
+            Console.WriteLine("¿Desea seguir?(s/n)");
+            string respuesta = Console.ReadLine();
+            Console.WriteLine("Ingrese el número de jugadores (min 2 max 5)");
+            int n = int.Parse(Console.ReadLine());
+            int[] puntajes = new int[n];
+            for(int i=0;i<puntajes.Length;i++)
+            {
+                Console.WriteLine("Bienvenido jugador " + i + 1);
+                int carta1 = aleatorio.Next(1, 11);
+                int carta2 = aleatorio.Next(1, 11);
+                Console.WriteLine("Su primera carta señor: " + carta1);
+                Console.WriteLine("Su segunda carta señor: " + carta2);
+                int ncarta;
+                int total = carta1 + carta2;
+                Console.WriteLine("Total: " + total);
+                while(total<21)
+                {
+                    Console.WriteLine("¿Desea una nueva carta?");
+                    mascartas = Console.ReadLine();
+                    if (mascartas != "s") break;
+                    ncarta = aleatorio.Next(1, 11);
+                    total += ncarta;
+                    Console.WriteLine("Nueva carta: " + ncarta);
+                    Console.WriteLine("total: " + total);
+                    if(total==21)
+                    {
+                        Console.WriteLine("Ganaste!");
+                        break;
+                    }
+                    if (total>21)
+                    {
+                        Console.WriteLine("¡Perdiste!");
+                        Console.WriteLine("Te pasaste de 21");
+                        total = 0;
+                        Console.WriteLine("total" + total);
+                        break;
+                    }
+                   
+                }
+                puntajes[i] = total;
+            }
+            int max = 0, segundo = 0, nomax = 0, nom2 = 0;
+            for (int i = 0; i < puntajes.Length; i++)
+            {
+                if (puntajes[i] > max)
+                {
+                    max = puntajes[i];
+                    nomax = i;
+                }
+            }
+            for(int j=0;j<puntajes.Length;j++)
+            {
+                if(puntajes[j]>segundo&&puntajes[j]<max)
+                {
+                    segundo = puntajes[j];
+                    nom2 = j;
+                }
+            }
+            Console.WriteLine("Primer puesto. jugador# " + nomax + 1 + "puntaje de" + puntajes[nomax]);
+            Console.WriteLine("Segundo puesto. jugador# " + segundo + 1 + "puntaje de" + puntajes[nom2]);
+        }
+    }
+}
---
+++ nombresyestaturas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace nombresyestaturas
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            string[] nombres = { "Nezuko", "Tanjiro", "Zenitsu", "Inosuke", "Genya", "Kanao", "Tomioka" };
+            double[] estatura = { 153, 165, 164.5, 164, 180, 156, 176 };
+
+            for(int j = 0; j < estatura.Length; j++)
+            {
+                for (int i = 0; i < estatura.Length - 1; i++)
+                {
+                    double tmp = estatura[i];
+                    estatura[i] = estatura[i + 1];
+                    estatura[i + 1] = tmp;
+                    
+                }
+                Console.WriteLine(estatura[j]);
+                
+            }
+            for (int j = 0; j < nombres.Length; j++)
+            {
+                for (int i = 0; i < nombres.Length - 1; i++) 
+                {
+                    string tmp = nombres[i];
+                    nombres[i] = nombres [i + 1];
+                    nombres[i + 1] = tmp;
+
+                }
+                Console.WriteLine(nombres[j]);
+
+            }
+
+        }
+    }
+}
