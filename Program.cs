8331d908c7561072753a6fe5e337c746e6473914
Mariaalejagomezb <60151417+Mariaalejagomezb@users.noreply.github.com>
2020-09-11 16:22:09 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,49 +1,61 @@
 ﻿using System;
 
-namespace Dado_vida_puntos
+namespace Matrices_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int dado = 0, dado2 = 0, vidas = 3, turnos = 0, total = 0;
-            string respuesta = "";
-            Random aleatorio= new Random();
-            while (respuesta=="s")
-            {
-                
-                Console.WriteLine("¿tirar dado?(s/n)");
-                respuesta = Console.ReadLine();
-                dado = aleatorio.Next(1, 7);
-                Console.WriteLine("dado:" + dado);
-                turnos++;
-                total += dado;
-                  if (turnos % 2 == 0)
-                  {
-                    vidas--;
-                  }
-                   
-                if (respuesta != "s") break;
-                if (turnos %3==0)
-                {
-                    Console.WriteLine("Puedes usar dos dados");
-                    dado2 = aleatorio.Next(1, 7);
-                    if (dado == dado2) vidas++;
-                    total += (dado + dado2);
-                }
-                if(total>=100)
-                {
-                    Console.WriteLine("Ganaste!");
-                    break;
-                }
-                if(vidas==0)
-                {
-                    Console.WriteLine("Te quedaste sin vidas");
-                    break;
-                }
-                
+            
+            Console.WriteLine("ingrese el número de filas");
+            int filas = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el número de columnas");
+            int columnas = int.Parse(Console.ReadLine());
 
+            //datos primera matriz
+            string[,] entrada = new string[filas, columnas];
+            for (int i = 0; i <filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    entrada[i, j] = "-";
+                }
             }
+            //datos segunda matriz (ingresar)
+            string[,] salida = new string[filas, columnas];
+            for (int i = 0; i <filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                   
+                    Console.Write("\nIngrese el valor en [" + i + "," + j + "] (X): ");
+                    salida[i, j] = Console.ReadLine();
+                    if (salida[i, j] == "X") Console.WriteLine("El valor [" + i + "," + j + "] ya está ocupado, pruebe con otra casilla");
+                    
+                }
+            }
+            //datos primera matriz (mostrar)
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.Write("|" + entrada[i, j]);
+                }
+                Console.Write("|\n");
+            }
+            Console.Write("\n");
+            //datossegunda matriz (mostrar)
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.Write("|" + salida[i, j]);
+                }
+                Console.Write("|\n");
+            }
+
+
+
         }
     }
 }
