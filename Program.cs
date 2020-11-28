5d6dc03ddd893caccc647cbcde8621470a9096ac
Mariaalejagomezb <60151417+Mariaalejagomezb@users.noreply.github.com>
2020-11-27 19:05:55 -0500

    Add files via upload
    seno for

--- Program.cs
+++ Program.cs
@@ -1,61 +1,22 @@
 ﻿using System;
 
-namespace Matrices_2
+namespace seno
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("ingrese el número de filas");
-            int filas = int.Parse(Console.ReadLine());
-            Console.WriteLine("ingrese el número de columnas");
-            int columnas = int.Parse(Console.ReadLine());
-
-            //datos primera matriz
-            string[,] entrada = new string[filas, columnas];
-            for (int i = 0; i <filas; i++)
+            double x = 1.25, seno = 0;
+            for(int i=0;i<100;i++)
             {
-                for (int j = 0; j < columnas; j++)
-                {
-                    entrada[i, j] = "-";
-                }
+                seno += ((Math.Pow(-1, i) / Factorial(2 * i + 1))*(Math.Pow(x,(2*i+1))));
+                Console.WriteLine("Seno: " + seno);
             }
-            //datos segunda matriz (ingresar)
-            string[,] salida = new string[filas, columnas];
-            for (int i = 0; i <filas; i++)
-            {
-                for (int j = 0; j < columnas; j++)
-                {
-                   
-                    Console.Write("\nIngrese el valor en [" + i + "," + j + "] (X): ");
-                    salida[i, j] = Console.ReadLine();
-                    if (salida[i, j] == "X") Console.WriteLine("El valor [" + i + "," + j + "] ya está ocupado, pruebe con otra casilla");
-                    
-                }
-            }
-            //datos primera matriz (mostrar)
-            for (int i = 0; i < filas; i++)
-            {
-                for (int j = 0; j < columnas; j++)
-                {
-                    Console.Write("|" + entrada[i, j]);
-                }
-                Console.Write("|\n");
-            }
-            Console.Write("\n");
-            //datossegunda matriz (mostrar)
-            for (int i = 0; i < filas; i++)
-            {
-                for (int j = 0; j < columnas; j++)
-                {
-                    Console.Write("|" + salida[i, j]);
-                }
-                Console.Write("|\n");
-            }
-
-
-
+        }
+        static double Factorial(double sumatoriafacto)
+        {
+            if (sumatoriafacto <= 1) return 1; 
+            return sumatoriafacto * Factorial(sumatoriafacto - 1);
         }
     }
 }
